7b0a1b76642f0b70f1578b307e480159b6334618
Thiago Roberto <thiago.roberto@techmagic.co>
2025-05-07 00:43:48 -0300

    fix(llm-fe): less agressive build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // enables static HTML export
+    output: 'export',
 };
 
 export default nextConfig;
